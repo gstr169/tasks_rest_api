19ff67959b351d923a862dc775cf319f0a3e0050
Dmitry Finko <dmit.finn@yandex.ru>
2019-02-10 15:00:43 +0300

    Added .gitignore

--- tasks_rest_api/settings.py
+++ tasks_rest_api/settings.py
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'tasks_rest_api.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
